accf6f18bbfaea936c6a8b11d01b35afe30ad51e
Komiljon Akbarov <komil79@yandex.ru>
2024-08-01 22:24:24 +0300

    les4 v1

--- app/handlers.py
+++ app/handlers.py
@@ -1,23 +1,24 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+import app.keybords as kb
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f'Hello! {message.from_user.first_name}')
+    await message.reply(f'Hello! {message.from_user.first_name}', reply_markup=kb.main)
 
 
 @router.message(Command('help'))
 async def get_help(message: Message):
-    await message.answer('Помощь!')
+    await message.answer('Помощь!', reply_markup=kb.settings)
 
 
-@router.message(F.text == 'Как дела?')
+@router.message(F.text == 'Cars?')
 async def ask_t(message: Message):
-    await message.answer('Я в порядке.')
+    await message.answer('Cars.', reply_markup=await kb.inline_cars())
 
 
 @router.message(F.photo)
---
+++ app/keybords.py
@@ -0,0 +1,22 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Catalog')],
+    [KeyboardButton(text='Basket'), KeyboardButton(text='Contacts')]
+],
+    resize_keyboard=True,
+    input_field_placeholder='Выбрать из меню')
+
+settings = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Aptechestvo', url='https://aptechestvo.ru')]
+])
+
+cars = ['Mers', 'BMW', 'Audi', 'Tesla']
+
+
+async def inline_cars():
+    keybard = InlineKeyboardBuilder()
+    for car in cars:
+        keybard.add(InlineKeyboardButton(text=car, url='https://aptechestvo.ru'))
+    return keybard.adjust(2).as_markup()
