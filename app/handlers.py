7932bcee50117a9c68461a764985d1eddecaf5e2
Komiljon Akbarov <komil79@yandex.ru>
2024-08-04 17:39:07 +0300

    les5 callback_query v1

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 import app.keybords as kb
 
 router = Router()
@@ -24,3 +24,9 @@ async def ask_t(message: Message):
 @router.message(F.photo)
 async def get_photo(message: Message):
     await message.answer('Это картинка.')
+
+
+@router.callback_query(F.data == 'catalog')
+async def catalog(callback: CallbackQuery):
+    await callback.answer('fdsfdsfs', show_alert=True)
+    await callback.message.edit_text('Catalog chous', reply_markup=await kb.inline_cars())
--- app/keybords.py
+++ app/keybords.py
@@ -1,9 +1,18 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Catalog')],
-    [KeyboardButton(text='Basket'), KeyboardButton(text='Contacts')]
+
+# main = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text='Catalog')],
+#     [KeyboardButton(text='Basket'), KeyboardButton(text='Contacts')]
+# ],
+#     resize_keyboard=True,
+#     input_field_placeholder='Выбрать из меню')
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Catalog', callback_data='catalog')],
+    [InlineKeyboardButton(text='Basket', callback_data='basket'),
+     InlineKeyboardButton(text='Contacts', callback_data='contacts')]
 ],
     resize_keyboard=True,
     input_field_placeholder='Выбрать из меню')
